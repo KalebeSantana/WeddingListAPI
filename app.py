13e9bcba0ba119930c40f3e83b7007a08ab36f8b
KalebeSantana <santana.abrantes01@gmail.com>
2024-05-16 20:37:56 -0300

    Adicionada rota para envio de email e rota para produto direto

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 import os
 from dotenv import find_dotenv, load_dotenv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Find .env file
 dotenv_path = find_dotenv()
@@ -26,6 +29,29 @@ conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
     port=os.getenv("DB_PORT")
 )
+
+def send_email(subject, message):
+    from_email = os.getenv('EMAIL_USER')
+    from_password = os.getenv('EMAIL_PASS')
+    to_email = os.getenv('TO_EMAIL_USER')
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_email, from_password)
+        text = msg.as_string()
+        server.sendmail(from_email, to_email, text)
+        server.quit()
+        print("Email sent successfully")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
 # Rota para autenticação e obtenção de token
 @app.route('/login', methods=['POST'])
@@ -66,6 +92,30 @@ def listar_produtos():
         produtos_json.append(produto_dict)
 
     return jsonify(produtos_json)
+
+@app.route('/produtos/<int:id>', methods=['GET'])
+@jwt_required()
+def obter_produto_por_id(id):
+    current_user = get_jwt_identity()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM lista_de_presentes WHERE id = %s;", (id,))
+    produto = cursor.fetchone()  # Retorna apenas um produto correspondente ao ID
+
+    if not produto:
+        abort(404, f'Produto com ID {id} não encontrado.')
+
+    produto_dict = {
+        'id': produto[0],
+        'categoria': produto[1],
+        'nome': produto[2],
+        'descricao': produto[3],
+        'valor': float(produto[4]),
+        'link_compra': produto[5],
+        'comprado': produto[6]
+    }
+
+    cursor.close()
+    return jsonify(produto_dict), 200
 
 @app.route('/produtos', methods=['POST'])
 @jwt_required()
@@ -126,6 +176,28 @@ def deletar_produto(id):
     cursor.close()
 
     return 'Produto deletado com sucesso!', 200
+
+@app.route('/send-email', methods=['POST'])
+@jwt_required()
+def enviar_email():
+    current_user = get_jwt_identity()
+    try:
+        dados = request.json
+        if 'nome_usuario' not in dados or 'nome_produto' not in dados:
+            abort(400, 'Dados incompletos para enviar o email.')
+
+        nome_usuario = dados['nome_usuario']
+        nome_produto = dados['nome_produto']
+
+        subject = "Confirmação de Compra"
+        message = f"Compra do produto {nome_produto} foi confirmada por {nome_usuario}."
+        
+        send_email(subject, message)
+
+        return jsonify({'message': 'Email enviado com sucesso'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run()
